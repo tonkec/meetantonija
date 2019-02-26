f525d6d90b917bb12f5392d2c0743bb07e92c73d
Antonija Simic <antonija1023@gmail.com>
2019-02-26 15:22:06 +0100

    readd loader

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,10 @@
+$(window).bind("load", function() {
+  $("body").addClass("no-scroll");
+  $('.spinner').fadeOut(500, () => {
+    $("body").removeClass("no-scroll");
+  });
+});
+
 $(document).ready(function() {
   $(function() {
     var canvas = document.createElement("canvas");
