299d0b95538e164ebb6d58437de64c9fb78e066c
Antonija Simic <antonija1023@gmail.com>
2019-02-26 15:13:29 +0100

    add loader

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,10 @@
 import "modules/cursor";
 import "modules/email";
 import "modules/footer";
+
+$(window).bind("load", function() {
+  $("body").addClass("no-scroll");
+  $('.spinner').fadeOut(500, () => {
+    $("body").removeClass("no-scroll");
+  });
+});
