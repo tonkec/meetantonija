a6f230b93063633761290bf805dd7d60235508a1
Antonija Simic <antonija1023@gmail.com>
2019-05-17 20:27:32 +0200

    add manually js files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,3 @@
-import "modules/cursor";
-import "modules/email";
-import "modules/footer";
-
 $(window).bind("load", function() {
   $("body").addClass("no-scroll");
   $(".spinner").fadeOut(500, () => {
