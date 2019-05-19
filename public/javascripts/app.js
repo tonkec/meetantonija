05bd4f6db11c31fee4915031a8633604dab593ce
Antonija Simic <antonija1023@gmail.com>
2019-05-19 23:33:29 +0200

    minify html,css and js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,85 +1,73 @@
+function calculateHeight() {
+  var e = $(".footer-section").outerHeight(),
+    t = $(".footer-section").prev();
+  $(t).css({ "margin-bottom": e });
+}
 $(function() {
-  var canvas = document.createElement("canvas");
-  canvas.width = 24;
-  canvas.height = 24;
-  var centerX = canvas.width / 2;
-  var centerY = canvas.height / 2;
-  var radius = 10;
-  var context = canvas.getContext("2d");
-  context.beginPath();
-  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context.fillStyle = "transparent";
-  context.fill();
-  context.lineWidth = 4;
-  context.strokeStyle = "#292929";
-  context.stroke();
-  var dataURLBlack = canvas.toDataURL("image/png");
-  $("body, .left").css("cursor", "url(" + dataURLBlack + "), auto");
-
-  context.strokeStyle = "#f9c80e";
-  context.stroke();
-  var dataURLYellow = canvas.toDataURL("image/png");
+  var e = document.createElement("canvas");
+  (e.width = 24), (e.height = 24);
+  var t = e.width / 2,
+    o = e.height / 2,
+    a = e.getContext("2d");
+  a.beginPath(),
+    a.arc(t, o, 10, 0, 2 * Math.PI, !1),
+    (a.fillStyle = "transparent"),
+    a.fill(),
+    (a.lineWidth = 4),
+    (a.strokeStyle = "#292929"),
+    a.stroke();
+  var n = e.toDataURL("image/png");
+  $("body, .left").css("cursor", "url(" + n + "), auto"),
+    (a.strokeStyle = "#f9c80e"),
+    a.stroke();
+  var s = e.toDataURL("image/png");
   $("footer, .right, .portfolio-header, .portfolio-image-section").css(
     "cursor",
-    "url(" + dataURLYellow + "), auto"
+    "url(" + s + "), auto"
   );
-});
-
-$(".btn-nav, .btn-close").click(() => {
-  $(".btn-nav").text((i, text) => (text === "MENU" ? "CLOSE" : "MENU"));
-  $("body, html").toggleClass("no-scroll");
-  $(".left, .right").toggleClass("show");
-});
-
-$("#form").on("submit", function(event) {
-  event.preventDefault(); // prevent reload
-  $(".msg").show();
-  $(".msg").append(`
-    <span class="text">Sending...</span>
-  `);
-  var formData = new FormData(this);
-  formData.append("service_id", "gmail");
-  formData.append("template_id", "template_4cXwGrgd");
-  formData.append("user_id", "user_LOSuz8pR7cowgxlm0LOa5");
-
-  $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
-    type: "POST",
-    data: formData,
-    contentType: false, // auto-detection
-    processData: false // no need to parse formData to string
-  })
-    .done(function() {
-      $(".text").replaceWith(`
-        Message Sent! I will get back to you soon 🕶
-      `);
-    })
-    .then(() => {
-      setTimeout(() => {
-        $(".msg").remove();
-      }, 4000);
-    })
-    .fail(function(error) {
-      console.log("Oops... " + JSON.stringify(error));
-    });
-});
-
-function calculateHeight() {
-  var h = $(".footer-section").outerHeight();
-  var section = $(".footer-section").prev();
-  $(section).css({ "margin-bottom": h });
-}
-
-$(document).ready(() => {
-  calculateHeight();
-});
-
-$(window).resize(() => {
-  calculateHeight();
-});
-
-$(window).bind("load", function() {
-  $("body").addClass("no-scroll");
-  $(".spinner").fadeOut(200, () => {
-    $("body").removeClass("no-scroll");
+}),
+  $(".btn-nav, .btn-close").click(() => {
+    $(".btn-nav").text((e, t) => ("MENU" === t ? "CLOSE" : "MENU")),
+      $("body, html").toggleClass("no-scroll"),
+      $(".left, .right").toggleClass("show");
+  }),
+  $("#form").on("submit", function(e) {
+    e.preventDefault(),
+      $(".msg").show(),
+      $(".msg").append('\n    <span class="text">Sending...</span>\n  ');
+    var t = new FormData(this);
+    t.append("service_id", "gmail"),
+      t.append("template_id", "template_4cXwGrgd"),
+      t.append("user_id", "user_LOSuz8pR7cowgxlm0LOa5"),
+      $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
+        type: "POST",
+        data: t,
+        contentType: !1,
+        processData: !1
+      })
+        .done(function() {
+          $(".text").replaceWith(
+            "\n        Message Sent! I will get back to you soon 🕶\n      "
+          );
+        })
+        .then(() => {
+          setTimeout(() => {
+            $(".msg").remove();
+          }, 4e3);
+        })
+        .fail(function(e) {
+          console.log("Oops... " + JSON.stringify(e));
+        });
+  }),
+  $(document).ready(() => {
+    calculateHeight();
+  }),
+  $(window).resize(() => {
+    calculateHeight();
+  }),
+  $(window).bind("load", function() {
+    $("body").addClass("no-scroll"),
+      $(".spinner").fadeOut(200, () => {
+        $("body").removeClass("no-scroll");
+      });
   });
-});
