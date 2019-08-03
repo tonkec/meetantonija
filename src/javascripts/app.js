010be6636d274ed6e100145f4132b4bda3e40798
Antonija Simic <antonija1023@gmail.com>
2019-08-03 15:42:00 +0200

    add pimsf; remove zoomero; add fotke folder

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -3,33 +3,6 @@ $(document).ready(() => {
 
   $(".left, .right").show();
 
-  $(function() {
-    var canvas = document.createElement("canvas");
-    canvas.width = 24;
-    canvas.height = 24;
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var radius = 10;
-    var context = canvas.getContext("2d");
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = "transparent";
-    context.fill();
-    context.lineWidth = 4;
-    context.strokeStyle = "#292929";
-    context.stroke();
-    var dataURLBlack = canvas.toDataURL("image/png");
-    $("body, .left").css("cursor", "url(" + dataURLBlack + "), auto");
-
-    context.strokeStyle = "#f9c80e";
-    context.stroke();
-    var dataURLYellow = canvas.toDataURL("image/png");
-    $("footer, .right, .portfolio-header, .portfolio-image-section").css(
-      "cursor",
-      "url(" + dataURLYellow + "), auto"
-    );
-  });
-
   $(".btn-nav, .btn-close").click(() => {
     $(".btn-nav").text((i, text) => (text === "MENU" ? "CLOSE" : "MENU"));
     $("body, html").toggleClass("no-scroll");
