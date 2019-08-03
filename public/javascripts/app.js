819ca994b668f5c44edc60e60c3dd535babfc498
Antonija Simic <antonija1023@gmail.com>
2019-08-03 15:49:17 +0200

    add js files lol

---
+++ public/javascripts/app.js
@@ -0,0 +1,58 @@
+$(document).ready(() => {
+  calculateHeight();
+
+  $(".left, .right").show();
+
+  $(".btn-nav, .btn-close").click(() => {
+    $(".btn-nav").text((i, text) => (text === "MENU" ? "CLOSE" : "MENU"));
+    $("body, html").toggleClass("no-scroll");
+    $(".left, .right").toggleClass("show");
+  });
+
+  $("#form").on("submit", function(event) {
+    event.preventDefault(); // prevent reload
+    $(".msg").show();
+    $(".msg").append(`
+      <span class="text">Sending...</span>
+    `);
+    var formData = new FormData(this);
+    formData.append("service_id", "gmail");
+    formData.append("template_id", "template_4cXwGrgd");
+    formData.append("user_id", "user_LOSuz8pR7cowgxlm0LOa5");
+
+    $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
+      type: "POST",
+      data: formData,
+      contentType: false, // auto-detection
+      processData: false // no need to parse formData to string
+    })
+      .done(function() {
+        $(".text").replaceWith(`
+          Message Sent! I will get back to you soon 🕶
+        `);
+      })
+      .then(() => {
+        setTimeout(() => {
+          $(".msg").remove();
+        }, 4000);
+      })
+      .fail(function(error) {
+        console.log("Oops... " + JSON.stringify(error));
+      });
+  });
+
+  function calculateHeight() {
+    var h = $(".footer-section").outerHeight();
+    var section = $(".footer-section").prev();
+    $(section).css({ "margin-bottom": h });
+  }
+
+  $("body").addClass("no-scroll");
+  $(".spinner").fadeOut(200, () => {
+    $("body").removeClass("no-scroll");
+  });
+
+  $(window).resize(() => {
+    calculateHeight();
+  });
+});
