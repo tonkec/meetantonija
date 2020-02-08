971bb16dc50aaa1376a5e115dd932bd373e3fec8
Antonija Simic <antonija1023@gmail.com>
2020-02-08 01:07:01 +0100

    remove resume and meetup link

--- src/components/footer/FooterList.js
+++ src/components/footer/FooterList.js
@@ -2,15 +2,9 @@ import React from "react";
 import footerList from "./stylesheets/footerList";
 
 const links = [
-  {
-    name: "resume",
-    href:
-      "https://drive.google.com/file/d/1Xpp9JI6fI_QXVkzZDFOSY18A2eyJjeF7/view?usp=sharing"
-  },
   { name: "linkedin", href: "https://www.linkedin.com/in/antonija-simic/" },
   { name: "github", href: "https://github.com/tonkec" },
   { name: "codepen", href: "https://codepen.io/tonkec/" },
-  { name: "meetup", href: "https://www.meetup.com/S_CSS_Zagreb/" },
   { name: "blog", href: "https://mytwocents.dev/" }
 ];
 
