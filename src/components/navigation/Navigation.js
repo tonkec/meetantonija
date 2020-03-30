f9e6c2b8ecbe08e986c90ed86f8536a0090f85bc
Antonija Simic <antonija1023@gmail.com>
2020-03-30 19:33:42 +0200

    replace sidebar with navbar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import navigation from "./stylesheets/navigation";
-import navOpener from "./stylesheets/navOpener";
-import data from "./../data";
-import normalTitle from "./../helpers/normalTitle";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 class Navigation extends React.Component {
   state = {
     visible: false,
@@ -36,45 +30,47 @@ class Navigation extends React.Component {
     let { visible } = this.state;
     return (
       <>
-        <button
-          className={`${navOpener} button`}
-          onClick={this.toggleVisibility}
-        >
-          MENU
-        </button>
         <div
-          className={`${navigation} navigation__inner navigation__inner--left ${
+          className={`navigation__inner navigation__inner--right ${
             visible ? "show" : ""
           }`}
         >
           <ul className="navigation__list">
             <li>
-              <Link to="/" onClick={this.toggleVisibility} className="button">
+              <NavLink
+                exact
+                to="/"
+                activeStyle={{
+                  fontWeight: "bold"
+                }}
+              >
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
+                exact
                 to="/about"
-                onClick={this.toggleVisibility}
-                className="button"
+                activeStyle={{
+                  fontWeight: "bold"
+                }}
               >
                 About
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
+                exact
                 to="/contact"
-                onClick={this.toggleVisibility}
-                className="button"
+                activeStyle={{
+                  fontWeight: "bold"
+                }}
               >
                 Contact
-              </Link>
+              </NavLink>
             </li>
             <li>
               <a
-                onClick={this.toggleVisibility}
-                className="button"
                 href="https://mytwocents.dev"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -82,43 +78,6 @@ class Navigation extends React.Component {
                 Blog
               </a>
             </li>
-          </ul>
-        </div>
-        <div
-          className={`${navigation} navigation__inner navigation__inner--right ${
-            visible ? "show" : ""
-          }`}
-        >
-          <ul className="navigation__list">
-            {data.client.map((item, i) => {
-              let title = normalTitle(item.title);
-              return (
-                <li key={i}>
-                  <Link
-                    to={`${this.state.location}/${title}`}
-                    onClick={this.toggleVisibility}
-                    className="button"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })}
-
-            {data.hobby.map((item, i) => {
-              let title = normalTitle(item.title);
-              return (
-                <li key={i}>
-                  <Link
-                    to={`${this.state.location}/${title}`}
-                    onClick={this.toggleVisibility}
-                    className="button"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })}
           </ul>
         </div>
       </>
--- src/components/navigation/stylesheets/navigation.js
+++ src/components/navigation/stylesheets/navigation.js
@@ -1,52 +1,41 @@
-import { css } from "emotion";
-import { yellow, brown } from "./../../../stylesheets/variables";
-const nav = css({
-  "&.navigation__inner": {
-    height: "100%",
-    width: "50%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "fixed",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    zIndex: 99,
+// import { css } from "emotion";
+// import { yellow, brown } from "./../../../stylesheets/variables";
+// const nav = css({
+//   "&.navigation__inner": {
+//     height: "auto",
+//     width: "100%",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     position: "fixed",
+//     top: 0,
+//     right: 0,
+//     left: 0,
+//     zIndex: 99,
 
-    "&--left": {
-      backgroundColor: yellow,
-      transition: "transform 0.5s cubic-bezier(.42,0,.58,1) 0.3s",
-      transform: "translateY(-150%)",
-      "&.show": {
-        transform: "translateY(0)"
-      }
-    },
-    "&--right": {
-      backgroundColor: brown,
-      transition: "transform 0.5s cubic-bezier(.42,0,.58,1)",
-      right: 0,
-      left: "auto",
-      transform: "translateX(150%)",
-      "&.show": {
-        transform: "translateX(0)"
-      },
-      a: {
-        "&:hover": {
-          borderColor: yellow
-        }
-      }
-    }
-  },
+//     "&--right": {
+//       backgroundColor: brown,
+//       transition: "transform 0.5s cubic-bezier(.42,0,.58,1)",
 
-  ".navigation__list": {
-    width: "50%",
-    padding: 0,
-    textAlign: "center",
-    li: {
-      display: "inline-block"
-    }
-  }
-});
+//       a: {
+//         "&:hover": {
+//           borderColor: yellow
+//         }
+//       }
+//     }
+//   },
 
-export default nav;
+//   ".navigation__list": {
+//     textAlign: "right",
+//     display: "flex",
+//     li: {
+//       width: 150,
+
+//       a: {
+//         width: "100%"
+//       }
+//     }
+//   }
+// });
+
+// export default nav;
