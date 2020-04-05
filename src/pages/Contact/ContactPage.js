e97f9e29b4c97ff2fa8eb7199d4b724d40fdc7dd
Antonija Simic <antonija1023@gmail.com>
2020-04-05 16:58:56 +0200

    fix contact page

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -5,15 +5,7 @@ import Header from "./../../components/header";
 
 const ContactPage = props => (
   <section className="contactpage">
-    <Header>
-      <div className="col-8 offset-2 text-center">
-        <h1>
-          Let's work together on an interesting project with interesting
-          technologies!
-        </h1>
-      </div>
-    </Header>
-    <ContactContainer text={"Let's chat"} />
+    <ContactContainer text={"Let's chat!"} />
     <Footer />
   </section>
 );
