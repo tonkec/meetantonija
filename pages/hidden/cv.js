a5f7740f1ae3c6195cf1bda9deb679a96730b05f
Antonija Simic <antonija1023@gmail.com>
2022-06-08 10:07:13 +0200

    add search on cv page

--- pages/hidden/cv.js
+++ pages/hidden/cv.js
@@ -3,162 +3,53 @@ import Nav from "./../../components/Nav"
 import Footer from "./../../components/Footer"
 import { paragraph, heading2, heading4, heading5 } from "../../utils/typography"
 import Link from "next/link"
+import {useState} from "react";
 
-export default function CV(){
+export default function CV(props){
+    const {projects} = props;
+    const [filteredProjects, setFilteredProjects] = useState(projects)
+    const onSearch = (e) => {
+        const filteredProject = projects.filter((project) => {
+            if (e.target.value.trim() !== "") {
+                const nameOfTheClient = project.title.toLowerCase().startsWith(e.target.value.toLowerCase())
+                return nameOfTheClient;
+            } 
+        })
+        setFilteredProjects(filteredProject);
+
+        if (e.target.value.trim() === "") {
+            console.log("empty")
+            setFilteredProjects(projects)
+        }
+        return filteredProject
+    }
     return <>
         <Nav />
-        <Header title="Antonija's CV" subtitle="Find out more about me" />
+        <Header title="Antonija's CV" subtitle="Read more about my work" />
         <section className="bg-gray">
-            <input className="bg-transparent text-white placeholder-white w-full py-10 px-20 text-4xl border-none focus:outline-none" type="search" placeholder="Search by name of a client or name of a project" />
-       
+            <input onChange={onSearch} className="bg-transparent text-white placeholder-white w-full py-10 px-20 text-4xl border-none focus:outline-none" type="search" placeholder="Search by the name of a client" />
         </section>
         <section className="bg-ternary py-20 px-20">
             <h2 className={`${heading2}`}>Work history</h2>
 
-            <div>
-                <h4 className={`${heading4} underline`}><a href="https://kodiraonica.dev/" target="_blank">Kodiraonica</a></h4>
-                <p className={`${paragraph}`}>April 2021 - present</p>
-            </div>
+            {filteredProjects.map((project) =>{
+                return <div key={project.title}>
+                    <div>
+                        <h4 className={`${heading4} underline mb-5 mt-20`}><a href={project.link} target="_blank">{project.title}</a></h4>
+                        <p className={`${paragraph}`}>{project.date}</p>
+                    </div>
 
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">I founded a coding school where I teach Javasript and React.js. <br />Our goal is to code several javascript games and one React.js application</li>
+                    <div className="pl-10 mt-10 mb-5">
+                        <ul className="list-disc">
+                            {project.tasks.map((task, i) => <li key={i} className="text-2xl">{task}</li>)}
+                        </ul>
+                    </div>
 
-                </ul>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://www.humaninteraction.com/" target="_blank">Human</a></h4>
-                <p className={`${paragraph}`}>Dec 2021 - March 2022</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Working with React.js and Storybook</li>
-                    <li className="text-2xl">Delivering pixel perfect layout</li>
-                    <li className="text-2xl">Transforming figma design into React and Stencil.js arhitecture</li>
-                    <li className="text-2xl">Developing abstract reusable components</li>
-
-                </ul>
-
-                <Link href="/projects/span">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://www.opx.org/" target="_blank">Oslo Philanthropic Exchange</a></h4>
-                <p className={`${paragraph}`}>Apr 2021 - Sep 2021</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">My main task was to implement user interface into Angular platform</li>
-                    <li className="text-2xl">Expertise included CSS knowledge and Angular templating skills</li>
-                    <li className="text-2xl">Among tasks were writing of unit tests</li>
-                </ul>
-
-                <Link href="/projects/opx">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://www.formunauts.at/en/" target="_blank">Formunauts</a></h4>
-                <p className={`${paragraph}`}>Jan 2020 - Sep 2020</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Set a communication between the app and the RaiseNow API. Send data from the form to the API.</li>
-                    <li className="text-2xl">Respond to offline/online status</li>
-                    <li className="text-2xl">Develop API architecture using services, injectables, axios calls, observables</li>
-                    <li className="text-2xl">Integrate mpos device for payment with credit cards</li>
-                    <li className="text-2xl">Setup service worker for caching assets</li>
-                </ul>
-
-                <Link href="/projects/formunauts">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://www.asynclabs.co/" target="_blank">Async Labs</a></h4>
-                <p className={`${paragraph}`}>Jun 2019 - Jan 2020</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Develop domain driven design for application architecture</li>
-                    <li className="text-2xl">Using React.js and Redux with Thunk</li>
-                    <li className="text-2xl">Implement data visualisation graph using Mermaid.js</li>
-                    <li className="text-2xl">Integrate mpos device for payment with credit cards</li>
-                    <li className="text-2xl">The data was parsed from the api which was then passed to the Mermaid graph.</li>
-                    <li className="text-2xl">Implement fluid layout with react-grid-layout package.</li>
-                    <li className="text-2xl">Mentor other devs, show them cool css tricks and simple solutions.</li>
-                </ul>
-
-                <Link href="/projects/revuto">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://codeinstitute.net/global/" target="_blank">Code Institute</a></h4>
-                <p className={`${paragraph}`}>Nov 2017 - Sep 2020</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Develop UI with HTML&amp;Scss within Django architecture based on Learning Management System</li>
-                    <li className="text-2xl">Improve existing CSS codebase which led to better performance</li>
-                    <li className="text-2xl">Create reading, audio and video materials for front-end module: html, css, js, scss</li>
-                    <li className="text-2xl">Teach web fundamentals from HTML to Django architecture</li>
-                    <li className="text-2xl">Help students think like professional developers from the very start of their coding career</li>
-                </ul>
-
-                <Link href="/projects/codeinstitute">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://punkrockdev.com/" target="_blank">Punk Rock Dev</a></h4>
-                <p className={`${paragraph}`}>Dec 2017 - May 2019</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Connect UI to API using axios</li>
-                    <li className="text-2xl">Develop reusable components</li>
-                    <li className="text-2xl">Render data in graphs</li>
-                    <li className="text-2xl">Develop responsive layout</li>
-                    <li className="text-2xl">Fix cross-platform issues, iOS vs Android</li>
-                </ul>
-
-                <Link href="/projects/craftstrom">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
-
-            <div>
-                <h4 className={`${heading4} underline mt-20`}><a href="https://supersuperagency.com/" target="_blank">Super super studio</a></h4>
-                <p className={`${paragraph}`}>Jan 2015 - Jul 2015</p>
-            </div>
-
-            <div className="pl-10 mt-10">
-                <ul className="list-disc">
-                    <li className="text-2xl">Develop static websites following pixel-perfect and mobile-first approach with Bootstrap</li>
-                    <li className="text-2xl">Implement cross-browser and cross-device compatibility</li>
-                    <li className="text-2xl">Optimise code using Gulp</li>
-                    <li className="text-2xl">Develop responsive layout</li>
-                    <li className="text-2xl">Customise jQuery animations and third-party libraries such as: Swiper.js, Waypoints,js, Skrollr.js, Parallax.js</li>
-                </ul>
-
-                <Link href="/projects/peaks">
-                    <a className="underline mt-6 inline-block -ml-10">Read more</a>
-                </Link>
-            </div>
+                    <Link href={`/projects/${project.route.toLowerCase()}`}>
+                        <a className="underline"> Read More</a>
+                    </Link>
+                </div>
+            } )}
         </section>
 
         <section className="bg-secondary text-white py-20 px-20">
@@ -214,4 +105,15 @@ export default function CV(){
 
         <Footer />
     </>
+}
+
+import fsPromises from 'fs/promises';
+import path from 'path'
+export async function getServerSideProps(context) {
+    const filePath = path.join(process.cwd(), 'data/cv.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData);
+    return {
+      props: objectData
+    }
 }
