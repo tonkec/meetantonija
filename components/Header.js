2f3c1466905a335f09b307c467b9db58bf5ff34b
Antonija Simic <antonija1023@gmail.com>
2022-06-11 11:00:35 +0200

    parallax header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,26 @@
 import { heading1, heading4 } from "../utils/typography"
+import { useRef, useEffect, useState } from "react";
 import Link from "next/link"
 
 function Header({title, subtitle, link}){
+    const magicDiv = useRef(null)
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);
+    };
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
     return (
         <header className='py-[200px] overflow-auto'>
-            <div className="bg-primary w-full px-4 py-2 md:w-3/4 xl:px-10 xl:w-2/4 float-right">
+           <div ref={magicDiv} style={{transform: `translateY(${scrollPosition * -1}px)`}}>
+           <div className="bg-primary w-full px-4 py-2 md:w-3/4 xl:px-10 xl:w-2/4 float-right">
                 <h1 className={`${heading1} text-right leading-normal font-bold`}>
                     {title} 
                    
@@ -18,6 +34,7 @@ function Header({title, subtitle, link}){
                    </Link>}
                 </h4>
             </div>
+           </div>
         </header>
     )
 }
