ee2c885c80ebb3a1ad0089c77393e176f78e3513
antonijasimic <antonija1023@gmail.com>
2023-01-02 07:38:45 +0100

    add casumo to projects

--- data/projects.js
+++ data/projects.js
@@ -1,26 +1,34 @@
 export const projects = [
-    {
-      title: "Span",
-      subtitle: "A landing page built with storybook and Stencil.js.",
-      href: "span",
-      stack: "react"
-    },
-    {
-      title: "OPX",
-      subtitle: "An application that connects NGOs and their potential donors built with Angular.",
-      href: "opx",
-      stack: "angular"
-    },
-    {
-      title: "Formunauts",
-      subtitle: "An application that collects funding for NGOs built with Angular and RxJS.",
-      href: "formunauts",
-      stack: "angular"
-    },
-    {
-      title: "Revuto",
-      subtitle: "A React application for managing subscriptions.",
-      href: "revuto",
-      stack: "react"
-    }
-  ]
+  {
+    title: "Casumo",
+    subtitle: "An online casino based in Malta.",
+    href: "casumo",
+    stack: "react",
+  },
+  {
+    title: "Span",
+    subtitle: "A landing page built with storybook and Stencil.js.",
+    href: "span",
+    stack: "react",
+  },
+  {
+    title: "OPX",
+    subtitle:
+      "An application that connects NGOs and their potential donors built with Angular.",
+    href: "opx",
+    stack: "angular",
+  },
+  {
+    title: "Formunauts",
+    subtitle:
+      "An application that collects funding for NGOs built with Angular and RxJS.",
+    href: "formunauts",
+    stack: "angular",
+  },
+  {
+    title: "Revuto",
+    subtitle: "A React application for managing subscriptions.",
+    href: "revuto",
+    stack: "react",
+  },
+];
