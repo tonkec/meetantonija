275d0e02dee12a3b0418ab360d81872d2221be73
antonijasimic <antonija1023@gmail.com>
2023-01-26 11:53:27 +0100

    add shortcuts to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,38 @@
 export default function Footer() {
-    return (
-        <footer className="py-20 bg-black md:px-20 print-hidden">
-            <div className="flex flex-wrap lg:flex-nowrap justify-between items-center">
-                <a href="/" className="px-4 text-white">meetantonija.com</a>
+  return (
+    <footer className="py-20 bg-black md:px-20 print-hidden">
+      <div className="flex flex-wrap lg:flex-nowrap justify-between items-center">
+        <a href="/" className="px-4 text-white">
+          meetantonija.com
+        </a>
 
-                <div>
-                    <a href="https://www.linkedin.com/in/antonija-simic/" target="_blank" className="px-4 text-white">Linkedin</a>  
-                    <a href="https://github.com/tonkec" target="_blank" className="px-4 text-white">Github</a>  
-                    <a href="https://codepen.io/tonkec" target="_blank" className="px-4 text-white">Codepen</a>  
-                </div>
-            </div>
-        </footer>
-    )
+        <div>
+          <a
+            href="https://www.linkedin.com/in/antonija-simic/"
+            target="_blank"
+            className="px-4 text-white"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/tonkec"
+            target="_blank"
+            className="px-4 text-white"
+          >
+            Github
+          </a>
+          <a
+            href="https://codepen.io/tonkec"
+            target="_blank"
+            className="px-4 text-white"
+          >
+            Codepen
+          </a>
+        </div>
+      </div>
+      <div className="px-4 mt-6">
+        <p className="text-primary"> ctrl + F - ESC</p>
+      </div>
+    </footer>
+  );
 }
