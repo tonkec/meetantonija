bf5d4fd9e4d8073d8c0b07f2c5c6c323f770163b
antonijasimic <antonija1023@gmail.com>
2023-02-08 14:13:52 +0100

    add link to cover letter and underline all of the links

--- components/Footer.js
+++ components/Footer.js
@@ -2,29 +2,36 @@ export default function Footer() {
   return (
     <footer className="py-20 bg-black md:px-20 print-hidden">
       <div className="flex flex-wrap lg:flex-nowrap justify-between items-center">
-        <a href="/" className="px-4 text-white">
+        <a href="/" className="px-4 text-white underline hover:text-primary">
           meetantonija.com
         </a>
 
         <div>
           <a
+            href="https://gist.github.com/tonkec/344482591e09ab240458ebbebbc3dc1b"
+            target="_blank"
+            className="px-4 text-white underline hover:text-primary"
+          >
+            Cover letter
+          </a>
+          <a
             href="https://www.linkedin.com/in/antonija-simic/"
             target="_blank"
-            className="px-4 text-white"
+            className="px-4 text-white underline hover:text-primary"
           >
             Linkedin
           </a>
           <a
             href="https://github.com/tonkec"
             target="_blank"
-            className="px-4 text-white"
+            className="px-4 text-white underline hover:text-primary"
           >
             Github
           </a>
           <a
             href="https://codepen.io/tonkec"
             target="_blank"
-            className="px-4 text-white"
+            className="px-4 text-white underline hover:text-primary"
           >
             Codepen
           </a>
