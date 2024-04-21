df024e154561f4b59289673a9d557a1a9c8461d0
tonkec <antonija1023@gmail.com>
2024-04-21 09:29:06 +0200

    clean up loaders

--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -1,9 +1,7 @@
 import { useParams, useNavigate } from 'react-router-dom'
-import posts from '../../data/posts'
 import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import PostsImage from './PostsImage'
-import Loader from '../../components/Loader'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import './Posts.scss'
@@ -11,7 +9,6 @@ import './Posts.scss'
 const PostPage = () => {
   const navigate = useNavigate()
   const [text, setText] = useState('')
-  const [loading, setLoading] = useState(true)
   const { id } = useParams()
   useEffect(() => {
     import(`./../../data/notes/${id}.md`)
@@ -19,7 +16,6 @@ const PostPage = () => {
         fetch(res.default)
           .then((response) => response.text())
           .then((response) => {
-            setLoading(false)
             setText(response)
           })
           .catch((err) => {
@@ -31,18 +27,17 @@ const PostPage = () => {
   }, [id, navigate])
   return (
     <div className="post-page">
-      {loading && <Loader />}
       <ReactMarkdown
         children={text}
         components={{
-          h1({ children, ...props }) {
+          h1({ children }) {
             return (
               <div className="container">
                 <h1>{children}</h1>
               </div>
             )
           },
-          p({ children, ...props }) {
+          p({ children }) {
             return (
               <div className="container">
                 <p>{children}</p>
