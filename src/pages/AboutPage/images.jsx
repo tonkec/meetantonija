96ca48ba5adc4341364c2e269c143c4b9f64ee20
tonkec <antonija1023@gmail.com>
2024-08-25 12:27:11 +0200

    fix path

--- src/pages/AboutPage/images.jsx
+++ src/pages/AboutPage/images.jsx
@@ -2,12 +2,12 @@ import { rootImageUrl } from 'rootImageUrl'
 
 export const images = [
   {
-    src: rootImageUrl + 'images/about/bike.jpg',
+    src: rootImageUrl + 'about/bike.jpg',
     alt: 'go for a ride on my bike.',
     subtitle: 'Me on a Jarun lake resting after a bike ride.',
   },
   {
-    src: rootImageUrl + 'images/about/basketball.jpg',
+    src: rootImageUrl + 'about/basketball.jpg',
     alt: 'enjoy a game of basketball.',
     subtitle: 'Me exploring backetball courts all over Croatia.',
   },
