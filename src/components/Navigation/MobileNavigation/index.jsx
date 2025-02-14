d43828b9cdb9a0352b28af94138bc46f26d6e44a
tonkec <antonija1023@gmail.com>
2025-02-14 10:23:55 +0100

    update navigation colors

--- src/components/Navigation/MobileNavigation/index.jsx
+++ src/components/Navigation/MobileNavigation/index.jsx
@@ -11,11 +11,7 @@ const MobileNavigation = ({ isOpen, links, onClose, isSettings }) => {
     <div className="fixed flex flex-column space-between overflow-auto top-0 bottom-0 right-0 bg-pink-light z-999 mobile-nav w-full">
       <div className="flex flex-column links">
         {links.map((link, index) => (
-          <NavigationLink
-            key={index}
-            href={link.href}
-            buttonClassName="ternary"
-          >
+          <NavigationLink key={index} href={link.href}>
             <span className="flex flex-y-center flex-gap-small">
               {index}. {link.label} {link.icon}
             </span>
@@ -24,7 +20,7 @@ const MobileNavigation = ({ isOpen, links, onClose, isSettings }) => {
       </div>
 
       <button
-        className="w-full dark inline-block normal-font no-border-radius"
+        className="w-full ternary inline-block normal-font no-border-radius"
         onClick={() => {
           onClose()
         }}
