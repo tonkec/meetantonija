f3bbbfec3e89b92fc1f6106236ee2855d05892bf
tonkec <antonija1023@gmail.com>
2025-02-15 13:08:03 +0100

    fix grid

--- src/components/Cta/index.jsx
+++ src/components/Cta/index.jsx
@@ -1,6 +1,6 @@
 const Cta = () => {
   return (
-    <section className="small-padding large-margin-top">
+    <section className="small-padding large-margin-top hidden-mobile">
       <div className="container text-center large-padding bg-sky-blue border-radius">
         <h3 className="no-margin text-black">Still looking for something?</h3>
         <p className="small-margin-top small-margin-bottom text-black">
