1bfca6420d57a95f2711599b7094fa974f587104
tonkec <antonija1023@gmail.com>
2025-07-13 10:56:31 +0200

    fix image height

--- src/pages/HomePage/components/Header/index.jsx
+++ src/pages/HomePage/components/Header/index.jsx
@@ -23,7 +23,10 @@ const Header = () => {
           <h1 className={isMobile ? 'normal-font' : ''}>
             I'm <span className="accent">Antonija</span>,
           </h1>
-          <p className="small-margin-right">
+          <p
+            className="small-margin-right large-margin-bottom"
+            style={{ fontSize: isMobile ? '0.9rem' : '1.5rem' }}
+          >
             a frontend developer from{' '}
             <span data-tooltip-id="my-tooltip">Sveta Nedelja</span>, Croatia,
             passionate about building beautiful, user-friendly web experiences.
@@ -50,6 +53,7 @@ const Header = () => {
           className={
             isMobile ? '' : 'content-end flex flex-1 medium-margin-right'
           }
+          style={{ order: isMobile ? -1 : 2 }}
         >
           <Image
             src="https://avatars.githubusercontent.com/u/5020758?v=4"
@@ -57,7 +61,7 @@ const Header = () => {
             hasColoredBackground={isMobile ? false : true}
             style={{
               width: isExtraSmall ? '220px' : isMobile ? '320px' : 'auto',
-              height: isExtraSmall ? '220px' : isMobile ? '320px' : 'auto',
+              height: isExtraSmall ? '220px' : isMobile ? '320px' : '400px',
               marginTop: isMobile ? '20px' : '0px',
             }}
             className="border-radius-50"
