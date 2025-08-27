87725ece330da6d3dc58a461411cdf18428a7abf
tonkec <antonija1023@gmail.com>
2025-08-27 17:53:08 +0200

    refactor footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,24 +4,22 @@ import Logo from 'components/Logo'
 import './Footer.scss'
 import Emoji from 'components/Emoji'
 
-const Footer = () => {
-  return (
-    <footer>
-      <div className="container">
-        <Emoji />
-        <div className="flex flex-x-center small-margin-bottom medium-margin-top">
-          <Social />
-        </div>{' '}
-        <p className="text-center xs-margin-bottom">
-          Coded by Antonija Šimić with lots of{' '}
-          <FaHeart className="icon" color="red" /> and{' '}
-          <FaCoffee color="#6F4E37" className="icon" />
-        </p>
-        <a href="/" className="text-black font-normal">
-          <Logo />
-        </a>
-      </div>
-    </footer>
-  )
-}
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <Emoji />
+      <div className="flex flex-x-center small-margin-bottom medium-margin-top">
+        <Social />
+      </div>{' '}
+      <p className="text-center xs-margin-bottom">
+        Coded by Antonija Šimić with lots of{' '}
+        <FaHeart className="icon" color="red" /> and{' '}
+        <FaCoffee color="#6F4E37" className="icon" />
+      </p>
+      <a href="/" className="text-black font-normal">
+        <Logo />
+      </a>
+    </div>
+  </footer>
+)
 export default Footer
