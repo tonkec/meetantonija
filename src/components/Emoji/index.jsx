8f83fb722d2db6d6970251f80f6ab05277406779
tonkec <antonija1023@gmail.com>
2025-08-31 20:24:18 +0200

    refactor emoji

--- src/components/Emoji/index.jsx
+++ src/components/Emoji/index.jsx
@@ -1,26 +1,24 @@
 import './Emoji.scss'
 
-const Emoji = () => {
-  return (
-    <div className="container text-center small-padding-top small-padding-bottom">
-      <div className="wrapper">
-        <div className="face">
-          <div className="straight-hair"></div>
-          <div className="hair"></div>
-          <div className="hair"></div>
-          <div className="hair"></div>
-          <div className="eye eye-left">
-            <div className="eyebrow"></div>
-          </div>
-          <div className="eye eye-right">
-            <div className="eyebrow"></div>
-          </div>
+const Emoji = () => (
+  <div className="container text-center small-padding-top small-padding-bottom">
+    <div className="wrapper">
+      <div className="face">
+        <div className="straight-hair"></div>
+        <div className="hair"></div>
+        <div className="hair"></div>
+        <div className="hair"></div>
+        <div className="eye eye-left">
+          <div className="eyebrow"></div>
         </div>
-        <div className="body"></div>
-        <div className="laptop"></div>
+        <div className="eye eye-right">
+          <div className="eyebrow"></div>
+        </div>
       </div>
+      <div className="body"></div>
+      <div className="laptop"></div>
     </div>
-  )
-}
+  </div>
+)
 
 export default Emoji
